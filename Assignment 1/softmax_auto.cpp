ff81d7a6faec8c89d59e44952cdb31023f14bfcd
Faxy <emilianosescu@gmail.com>
2025-03-12 23:22:28 +0100

    added compiler hints for ignoring vector dependencies

--- Assignment 1/softmax_auto.cpp
+++ Assignment 1/softmax_auto.cpp
@@ -7,20 +7,23 @@
 
 void softmax_auto(const float *input, float *output, size_t K) {
     // Find the maximum to stabilize the computation of the exponential
+	size_t i;
     float max_val = -std::numeric_limits<float>::infinity();
-    for (size_t i = 0; i < K; ++i) {
+    for (i = 0; i < K; ++i) {
 		max_val = std::max(max_val, input[i]);
     }
 
     // computes all exponentials with the shift of max_val and the total sum
+	#pragma GCC ivdep
     float sum = 0.0f;
-    for (size_t i = 0; i < K; ++i) {
+    for (i = 0; i < K; ++i) {
         output[i] = std::exp(input[i] - max_val);
         sum += output[i];
     }
 
     // normalize by dividing for the total sum
-    for (size_t i = 0; i < K; ++i) {
+	#pragma GCC ivdep
+    for (i = 0; i < K; ++i) {
         output[i] /= sum;
     }
 }
